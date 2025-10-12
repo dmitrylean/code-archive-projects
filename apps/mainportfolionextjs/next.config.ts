b3837fae8753bdb752b107a218d9f69366175b8d
dmitrylean <adam.lean.92@gmail.com>
2025-10-12 17:49:08 +0800

    add files

---
+++ apps/mainportfolionextjs/next.config.ts
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      "upload.wikimedia.org",
+      "www.metal-archives.com",
+      "open.spotify.com",
+      "music.apple.com",
+      "youtube.com",
+    ],
+  },
+};
+
+export default nextConfig;
