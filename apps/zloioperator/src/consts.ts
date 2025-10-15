d8bf15e028d85c1daa253d46027a96ec96bc3bac
dmitrylean <adam.lean.92@gmail.com>
2025-10-15 18:50:22 +0800

    add files

---
+++ apps/zloioperator/src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Злой оператор";
+export const SITE_DESCRIPTION = "Блог об удаленке и работе в колл центрах";
