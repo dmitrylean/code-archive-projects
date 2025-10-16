69b69f986e67e7b7680c81239ed7791c62e7b180
dmitrylean <adam.lean.92@gmail.com>
2025-10-17 00:23:41 +0800

    delete

--- backend/bots/news-bot/config.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:380d37d95d91f851196ac396fbd1c3b9c1619ca3dff7570a049b450384082c2b
-size 299
--- backend/bots/news-bot/fetcher.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:773bd508cf067435411083df4bad8ec8d10fa4ccc598e72500ce131c774bde4d
-size 353
